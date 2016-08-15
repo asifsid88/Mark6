ea43b72248550633b0cdc88a51ee02e3b2c7a80c
Ashique Hussain <mhussaa@amazon.com>
2016-08-15 20:27:19 +0530

    Added dummy product page

--- templates/productpage.php
+++ templates/productpage.php
@@ -1,5 +1,32 @@
+<style type="text/css">
+    #mark6-main-container-wrapper {
+        top: 0;
+        margin: 1em;
+    }
+</style>
+
 <div id="mark6-main-container-wrapper">
     <div id="mark6-main-container">
-        product page goes here
+        <div class="pure-g">
+            <div class="pure-u-7-12" style="border: 1px solid blue;">
+                <div class="pure-g" style="border: 1px solid black;">
+                    <div class="pure-u-5-24" style="border: 1px solid red;">
+                        <p>
+                            One-One
+                        </p>
+                    </div>
+                    <div class="pure-u-19-24" style="border: 1px solid red;">
+                        <p>
+                            One-Two
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="pure-u-5-12" style="border: 1px solid blue;">
+                <p>
+                    Two
+                </p>
+            </div>
+        </div>
     </div>
 </div>
