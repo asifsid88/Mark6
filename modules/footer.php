cc3b8027d737b828570a7193310c01a1d3a56ea8
Ashique Hussain <mhussaa@amazon.com>
2016-08-25 14:41:41 +0530

    Added links to follow-socials

--- modules/footer.php
+++ modules/footer.php
@@ -2,9 +2,9 @@
     <div id="footer-text">
         <div class="follow float-right">
             <div class="keep-in-touch">Keep in touch</div>
-            <div class="social facebook"></div>
-            <div class="social instagram"></div>
-            <div class="social twitter"></div>
+            <div class="social facebook" data-attr-link="https://www.facebook.com/mark6clothing"></div>
+            <div class="social instagram" data-attr-link="https://www.instagram.com/mark6clothing/"></div>
+            <div class="social twitter" data-attr-link="https://twitter.com/mark6clothing"></div>
         </div>
         <div class="clear-both"></div>
         <div>
@@ -13,3 +13,11 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+$('.social').on('click', function() {
+    var url = $(this).attr('data-attr-link');
+    var win = window.open(url, '_blank');
+    win.focus();
+});
+</script>
