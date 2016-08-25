125c50f0df3b79278009930eb335839f8cbb80e2
Ashique Hussain <mhussaa@amazon.com>
2016-08-25 17:37:30 +0530

    Added branded page (temp)

---
+++ brandpage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Mark6 Clothing" />
+    <meta name="google-site-verification" content="r5ASN538QC4ABCu9t73V3UsdJMwKSM6jSq1NdJxGK7Y" />
+    <title>Mark6</title>
+    <link rel="shortcut icon" href="src/img/favicon.ico" type="image/x-icon" />
+
+    <style type="text/css">
+        * {
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        body {
+            overflow-x: hidden;
+            display: block;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+
+        html > body {
+            font-size: 80%;
+            margin: 0;
+            background: #FFFEFE;
+            background-size: cover;
+        }
+
+        .brand-logo {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            margin-top: -50px;
+            margin-left: -100px;
+        }
+    </style>
+</head>
+<body style="background-color: #1E5247">
+    <div class="brand-logo">
+        <img src="src/img/brand_logo.png" /><br/>
+        <center>Enter >></center>
+    </div>
+</body>
+</html>
