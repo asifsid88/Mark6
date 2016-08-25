827fe3a146df0a527ec48da028b8cccc45bfb237
Ashique Hussain <mhussaa@amazon.com>
2016-08-25 18:06:47 +0530

    Added contact us page

--- modules/contactus.php
+++ modules/contactus.php
@@ -1,1 +1,39 @@
-this is the contact us page
+<div id="mark6-contactus-page">
+    <div id="mark6-tabs-panel">
+        <div class="mark6-tabs mark6-tabs-active">Contact Us</div>
+    </div>
+
+    <div id="mark6-tabs-content-panel">
+        <div class="title" style="margin-top: 2em;">
+            Mohammed Arif Siddiqui
+        </div>
+        <div class="description">
+            <table class="contact">
+                <tr>
+                    <td>Mobile Number</td>
+                    <td>0091 9004 522 900</td>
+                </tr>
+                <tr>
+                    <td>eMail</td>
+                    <td>arif@mark6traders.com</td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="title">
+            Enquiries
+        </div>
+        <div class="description">
+            <table class="contact">
+                <tr>
+                    <td>Customer Support</td>
+                    <td>customer@mark6traders.com</td>
+                </tr>
+                <tr>
+                    <td>Order</td>
+                    <td>order@mark6traders.com</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
--- templates/contactuspage.php
+++ templates/contactuspage.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="src/css/contactus-page.css" />
+
 <div id="mark6-main-container-wrapper">
     <div id="mark6-main-container">
         <?php include('modules/contactus.php'); ?>
