5c92d0a1a0d15ca60a99c8197fe738e6fd32fc4c
Ashique Hussain <mhussaa@amazon.com>
2016-08-25 17:24:01 +0530

    About us page completion

--- modules/aboutus.php
+++ modules/aboutus.php
@@ -1,1 +1,68 @@
-This is the aboutus page
+<div id="mark6-aboutus-page">
+    <div id="mark6-tabs-panel">
+        <div class="mark6-tabs mark6-tabs-active">About Us</div>
+    </div>
+
+    <div id="mark6-tabs-content-panel">
+        <div class="description" style="margin-top: 2em;">
+            The Mumbai based <strong>Mark6 Traders</strong>, is a highly accomplished creative professional in the field of Apparel manufacturing and designing.
+            With a focus on customer satisfaction, Mark6 Traders have earned an impeccable reputation for delivering quality products.
+            Today we match the international quality standards. We offer an elegant range of knitwear garments that comes in variety of designs and hues.
+        </div>
+
+        <div class="title">
+            Our Vision Statement
+        </div>
+        <div class="description">
+            To become a globally prominent Knitted garment manufacturer and be ranked amongst the top garment manufacturers
+            in the world in terms of quality, service standards and ultimately customer satisfaction.
+        </div>
+
+        <div class="title">
+            Our Mission Statement
+        </div>
+        <div class="description">
+            Through Commitment to people, system and technology, we lead the way in providing value to our customers and
+            other stakeholders by producing creative apparel solution in safe environment.
+        </div>
+
+        <div class="title">
+            Products
+        </div>
+        <div class="description">
+            We manufacture all types of apparels and owner and sole manufacturer of <strong>Mark6</strong>. The range of knitted clothing
+            and other products are made by using the finest fabrics which are in harmony with latest trends.
+            The design and workmanship range from the most intricate to extremely subtle to suit the varied styles across
+            the globe. Significant value enhancements are made to the garments by our creativity and resolution to excel.
+        </div>
+
+        <div class="title">
+            Quality of Policy
+        </div>
+        <div class="description">
+            We in Mark6 Traders believe that quality is a vital ingredient in all walks of our life.
+        </div>
+
+        <div class="title">
+            Secret of Quality
+        </div>
+        <div class="description">
+            <ul>
+                <li>Quality is the outcome of passion towards the involved product.</li>
+                <li>Quality can be achieved through untiring march towards perfection.</li>
+                <li>Quality can be derived through fool-proof system of working.</li>
+                <li>Quality can be derived by applying continuous chasing through-out the period of processing involved.</li>
+            </ul>
+        </div>
+
+        <div class="title">
+            Infrastructure
+        </div>
+        <div class="description">
+            We have integrated our facilities keeping in tune with our product specialization. Every core technology and
+            manpower area has been systematically integrated. Quality being the pride of Mark6 Traders, the machines and
+            men are chosen based on scientific evaluation. Today, Mark6 Traders has the best of technology and the best
+            of manpower working seamlessly to deliver best knitted Garments.
+        </div>
+    </div>
+</div>
