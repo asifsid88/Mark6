83f62f36fa078f11edb6a0c1699018c60fa9f1a6
Mohammed Asif <asifsid88@users.noreply.github.com>
2016-09-03 21:43:58 +0530

    Updated menu-bar for mobile version

--- modules/menubar.php
+++ modules/menubar.php
@@ -2,7 +2,7 @@
     <div class="pure-u-1 pure-u-md-1-2 vertical-center">
         <div class="pure-menu">
             <a href="/index.php" class="pure-menu-heading custom-brand"><img src="src/img/logo.png" id="mark6-logo" /></a>
-            <a class="custom-toggle" id="toggle"><s class="bar"></s><s class="bar"></s></a>
+            <a class="custom-toggle" id="toggle">Menu</a>
         </div>
     </div>
     <div class="pure-u-1 pure-u-md-1-2 vertical-center" id="menu-items">
