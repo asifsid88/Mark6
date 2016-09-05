7c38909d3ccb046c4a501f73ed65105631838101
Ashique Hussain <mhussaa@amazon.com>
2016-09-05 20:38:28 +0530

    Fixed menu-bar to middle and fixed loading of slideshow - codeCredit: Prateek@

--- modules/menubar.php
+++ modules/menubar.php
@@ -1,18 +1,20 @@
 <div id="mark6-menu" class="custom-wrapper pure-g affix">
-    <div class="pure-u-1 pure-u-md-1-2 vertical-center">
-        <div class="pure-menu">
-            <a href="/index.php" class="pure-menu-heading custom-brand"><img src="src/img/logo.png" id="mark6-logo" /></a>
-            <a class="custom-toggle" id="toggle">Menu</a>
+    <div class="container">
+        <div class="pure-u-1 pure-u-md-1-2 vertical-center">
+            <div class="pure-menu">
+                <a href="/index.php" class="logo pure-menu-heading custom-brand"><img src="src/img/logo.png" id="mark6-logo" /></a>
+                <a class="custom-toggle" id="toggle">Menu</a>
+            </div>
         </div>
-    </div>
-    <div class="pure-u-1 pure-u-md-1-2 vertical-center" id="menu-items">
-        <div class="pure-menu pure-menu-horizontal custom-can-transform">
-            <ul class="pure-menu-list">
-                <li class="pure-menu-item"><a href="/index.php" class="pure-menu-link">Home</a></li>
-                <li class="pure-menu-item"><a href="/search.php" class="pure-menu-link">Catalog</a></li>
-                <li class="pure-menu-item"><a href="/aboutus.php" class="pure-menu-link">About Us</a></li>
-                <li class="pure-menu-item"><a href="/contactus.php" class="pure-menu-link">Contact Us</a></li>
-            </ul>
+        <div class="pure-u-1 pure-u-md-1-2 vertical-center" id="menu-items">
+            <div class="pure-menu pure-menu-horizontal custom-can-transform">
+                <ul class="pure-menu-list">
+                    <li class="pure-menu-item"><a href="/index.php" class="pure-menu-link">Home</a></li>
+                    <li class="pure-menu-item"><a href="/search.php" class="pure-menu-link">Catalog</a></li>
+                    <li class="pure-menu-item"><a href="/aboutus.php" class="pure-menu-link">About Us</a></li>
+                    <li class="pure-menu-item"><a href="/contactus.php" class="pure-menu-link">Contact Us</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
--- src/javascript/set-container-css.js
+++ src/javascript/set-container-css.js
@@ -1,6 +1,7 @@
 $(window).load(function() {
     var ulHeight = $('ul.slides').height() + 'px';
     $('#mark6-main-container-wrapper').css('top', ulHeight);
+    $('.slider_container').attr('style','-webkit-filter: none; -o-filter: none; -moz-filter: none; -ms-filter: none');
 });
 
 (function (window, document) {
